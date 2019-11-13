cd6aa4512104460db60e93c0bb34ee7242934045
CommissarWex <57690342+CommissarWex@users.noreply.github.com>
2019-11-13 15:41:24 +0000

    Create 00_defines.lua

---
+++ The_Chapel_Mutiplayer/common/defines/00_defines.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+NDefines.NDiplomacy.PEACE_COST_DEMAND_PROVINCE = 100
+NDefines.NDiplomacy.CLAIM_PEACE_COST_DIP_FRACTION = -0.9
+NDefines.NDiplomacy.CORE_PEACE_COST_DIP_FRACTION = -1
